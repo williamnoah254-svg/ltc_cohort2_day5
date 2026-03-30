172dbb0c27e5f4f5f37068501d547fab818db01b
williamnoah254-svg <williamnoah254@gmail.com>
2026-03-30 14:14:35 +0300

    my 5th commit

---
+++ javascript-tutorial/index.js
@@ -0,0 +1,37 @@
+
+// console.log("hello world");
+// let secondName= "Noah";
+// firstName="Mary";
+// const pi =3.142;
+// let isRaining = true;
+// let myAge = null;
+// let thirdName;
+
+// let fruits =["banana","apple","avocado"]
+// let album ={"artistName":"Benson Boone","albumName":"Tears","yearsofpublish":2014}
+
+// // arithmetic operators (+,-,/,*,%,**)
+// let a = 10;
+// let b = 20;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+// // comparison/relational operators(>,<,>=,<=,==,===)
+// // logical oparators(&&,||,!)
+
+// console.log(typeof(secondName));
+// console.log(typeof(fruits));
+// console.log(typeof(album));
+// console.log(typeof(thirdName));
+// console.log(typeof(myAge));
+// console.log(typeof(isRaining));
+
+let num1 =prompt("enter num1")
+let num2 =prompt("enter num2")
+
+let results;
+results = Number(num1)+Number(num2)
+console.log("your resluts is:",results)
