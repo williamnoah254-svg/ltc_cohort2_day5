1a018e71946db5b5828df4a849130988741df500
williamnoah254-svg <williamnoah254@gmail.com>
2026-04-01 14:10:17 +0300

    my 8th commit

--- javascript-tutorial/index.js
+++ javascript-tutorial/index.js
@@ -62,48 +62,106 @@
 // }
 
 
-// loop
-for(let  i=1;i<21;i++){
-    console.log(i)
-}
+// // loop
+// for(let  i=1;i<21;i++){
+//     console.log(i)
+// }
 
-// types  of loop
-// wild loop
-let counter=20;
-while(counter<30){
-    counter++;
-    console.log("loops are amazing")
-}
+// // types  of loop
+// // wild loop
+// let counter=20;
+// while(counter<30){
+//     counter++;
+//     console.log("loops are amazing")
+// }
 
-// do while loop
-let x=10
-    do{
-        console.log("i am happy");
-        x++;
-    }
-    while(x<30)
-
-
- // for in loop
-     fruits=["apple","banana","mangoes"]
-     for(let fruit of fruits)
-     {
-         console.log(fruits)
-     }
+// // do while loop
+// let x=10
+//     do{
+//         console.log("i am happy");
+//         x++;
+//     }
+//     while(x<30)
 
 
-    //  function declaration
-     function addNumber(a,b){
-        // return a+b;
-        let sum = a+b;
-        console.log(sum)
-     }
-     addNumber(4,6)
+//  // for in loop
+//      fruits=["apple","banana","mangoes"]
+//      for(let fruit of fruits)
+//      {
+//          console.log(fruits)
+//      }
 
-    //  funtion expression
-     const subnumbers = function(a,b){
-        let sub = a-b;
-        console.log(sub)
-     }
 
-     subnumbers(60,40)
+//     //  function declaration
+//      function addNumber(a,b){
+//         // return a+b;
+//         let sum = a+b;
+//         console.log(sum)
+//      }
+//      addNumber(4,6)
+
+//     //  funtion expression
+//      const subnumbers = function(a,b){
+//         let sub = a-b;
+//         console.log(sub)
+//      }
+
+//      subnumbers(60,40)
+
+
+// // fucntion Declaration
+// function addNumber(a,b){
+//     function a+b;
+//     return a+b;
+// }
+// console.log(addNumber(4,8));
+
+
+// // function expression
+// const subnumbers = (a,b){
+//     return a-b;
+// }
+// console.log(subNumbers(40,10))
+
+// // arrow function
+// const divNumbers =(a,b) =>{
+//     return a/b;
+// }
+// console.log(divNumbera(40,10))
+
+// let counter =0;
+// if(counter>10)
+// {
+//     let age =30;
+//     console.log(`my age is${age}`)
+// }
+
+
+// let el = document.querySelector(".p2")
+// console.log(el)
+
+// let el = document.querySelectorAll("p")
+// console.log(el)
+// el.textContent="hello world"
+// console.log(el)
+
+let fname ="john"
+let el = document.querySelector("h1")
+console.log(el)
+el.innerText= `hello ${fname}`
+el.classList.add("enlarge")
+
+let el2 = document.querySelector("div");
+const p =document.createElement("p")
+let paragraph = `Hello this a paragraph from javascrpt`
+p.innerText=paragraph;
+document.body.appendChild(p) 
+
+
+let el3 =document.querySelector(".p3")
+
+
+let el4=document.querySelector("button")
+el4.addEventListener("click",function(){
+el3.classList.add("hide")
+}) 
