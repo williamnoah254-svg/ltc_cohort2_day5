cad6dbbc6a32392e19a4ffe77f5674d5128f37fe
williamnoah254-svg <williamnoah254@gmail.com>
2026-04-09 14:00:56 +0300

    my 11th commit

--- dom_exercise/index.js
+++ dom_exercise/index.js
@@ -13,22 +13,81 @@
 // })
 
 
-// Object
-let artist = {
-    artistName: "james",
-    age:30,
-    song:"the night is still young"
-}
-console.log(artist.artistName)
-artist["artistName"] = "Mary";
-console.log(artist)
-// console.log(Object.keys(artist))
-Object.keys(artist).forEach(keys => console.log(keys))
-Object.values(artist).forEach(values => console.log(values))
-console.log(Object.entries(artist))
+// // Object
+// let artist = {
+//     artistName: "james",
+//     age:30,
+//     song:"the night is still young"
+// }
+// console.log(artist.artistName)
+// artist["artistName"] = "Mary";
+// console.log(artist)
+// // console.log(Object.keys(artist))
+// Object.keys(artist).forEach(keys => console.log(keys))
+// Object.values(artist).forEach(values => console.log(values))
+// console.log(Object.entries(artist))
 
-for (const[key,value] of Object.entries(artist))
-{
-    console.log(`key ${key} value : ${value}`)
-}
+// for (const[key,value] of Object.entries(artist))
+// {
+//     console.log(`key ${key} value : ${value}`)
+// }
 
+// asynchronous javascript & api
+// setTimeout(function,delayTime.ms)
+// console.log("task 1 completed")
+
+// setTimeout(()=>console.log("task 2 completed"),300)
+
+// console.log("task 3 completed")
+// console.log("task 4 completed")
+
+// const myPromise = new Promise((resolve,reject)=>{
+//     let succes = true;
+//     if(succes){
+//         resolve("The task was successifull")
+//     }
+//     else{
+//         reject("somthing went wrong")
+//     }
+// })
+// myPromise.then((result)=>console.log(result)).
+// catch((error)=>console.log(error))
+
+// // async/await
+// async function getData() {
+//     const output = await myPromise
+//     console.log(output)
+// }
+// getData()
+
+
+// MY NOTES
+// https://hoppscotch.io/
+// https://jsonplaceholder.typicode.com/
+// status code
+
+// Fetch API
+// console.log(fetch(`https://jsonplaceholder.typicode.com/posts/1`).then(Response => console.log(Response.json())))
+
+// fetch(`https://jsonplaceholder.typicode.com/posts/1`).then(resp => resp.json()).then((data) => {
+//     document.body.innerText= JSON.stringify(data)
+// })
+
+//    fetch(`https://jsonplaceholder.typicode.com/posts/1`).then(resp => resp.json()).then((data) => {
+   
+//    const{userId,id,title,body} = data
+//    console.log(title,body) = data
+// })
+
+async function getData() {
+    const resp = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+    const data = await resp.json()
+    const{userId,id,title,body} = data
+    
+    let heading =document.createElement("h1")
+    heading.innerText = data["title"]
+    document.body.appendChild(heading)
+    // console.log(data)
+    
+}
+getData()
